0fd6b010b86aa351800b9bb56e6766398da124ec
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-05-16 17:13:53 -0300

    fix tasks count

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,16 +10,19 @@ todo = requests.get(api_todo).json()
 user = requests.get(api_users).json()
 
 tasks = []
-tasksdone = 0
+tasks_done = 0
+tasks_count = 0
 
 for item in todo:
     if item["userId"] == int(argv[1]):
         if item["completed"]:
             tasks.append(item["title"])
-            tasksdone += 1
+            tasks_done += 1
+        tasks_count += 1
 
-print("Employee {} is done with tasks({}/{}):"
-      .format(user['name'], tasksdone, len(tasks)))
+print("Employee {} is done with tasks({}/{}):".format(user['name'],
+                                                      tasks_done,
+                                                      tasks_count))
 
 for task in tasks:
     print("\t " + task)
