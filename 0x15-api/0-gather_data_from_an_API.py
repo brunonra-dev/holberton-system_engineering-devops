1865e9deb6215eaa86b1c8607128976d75c55b22
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-05-16 17:21:24 -0300

    fix main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,25 +4,26 @@ import requests
 from sys import argv
 
 
-api_todo = "https://jsonplaceholder.typicode.com/todos/"
-api_users = "https://jsonplaceholder.typicode.com/users/" + argv[1]
-todo = requests.get(api_todo).json()
-user = requests.get(api_users).json()
+if __name__ == '__main__':
+    api_todo = "https://jsonplaceholder.typicode.com/todos/"
+    api_users = "https://jsonplaceholder.typicode.com/users/" + argv[1]
+    todo = requests.get(api_todo).json()
+    user = requests.get(api_users).json()
 
-tasks = []
-tasks_done = 0
-tasks_count = 0
+    tasks = []
+    tasks_done = 0
+    tasks_count = 0
 
-for item in todo:
-    if item["userId"] == int(argv[1]):
-        if item["completed"]:
-            tasks.append(item["title"])
-            tasks_done += 1
-        tasks_count += 1
+    for item in todo:
+        if item["userId"] == int(argv[1]):
+            if item["completed"]:
+                tasks.append(item["title"])
+                tasks_done += 1
+            tasks_count += 1
 
-print("Employee {} is done with tasks({}/{}):".format(user['name'],
-                                                      tasks_done,
-                                                      tasks_count))
+    print("Employee {} is done with tasks({}/{}):".format(user['name'],
+                                                          tasks_done,
+                                                          tasks_count))
 
-for task in tasks:
-    print("\t " + task)
+    for task in tasks:
+        print("\t " + task)
