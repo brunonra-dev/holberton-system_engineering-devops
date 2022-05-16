d8767be76e86cecca8418dee3cc554d0f706bcd0
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-05-16 17:50:53 -0300

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == '__main__':
+    api_todo = "https://jsonplaceholder.typicode.com/todos/"
+    api_users = "https://jsonplaceholder.typicode.com/users/" + argv[1]
+    todo = requests.get(api_todo).json()
+    user = requests.get(api_users).json()
+
+    tasks = []
+
+    for item in todo:
+        if item["userId"] == int(argv[1]):
+            task = []
+            task.append("{}".format(item["userId"]))
+            task.append("{}".format(user["username"]))
+            task.append("{}".format(item["completed"]))
+            task.append("{}".format(item["title"]))
+            tasks.append(task)
+
+    with open('{}.csv'.format(argv[1]), 'w') as f:
+        write = csv.writer(f, quoting=csv.QUOTE_ALL)
+        write.writerows(tasks)
